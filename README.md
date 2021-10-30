99922b271c15790a6d75b7209f4e0d90dd1cb2b8
Steven Shoemaker <stshoema@ucsd.edu>
2021-10-29 22:10:43 -0700

    An attempt to load the second set of recipies w/o the slash in their title

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab 6 - Starter
+
+### Group Members
+- Steven Shoemaker
+
+### Link to Github Pages
+https://ucsdsteveshoe.github.io/Lab6_Starter/
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,9 +6,9 @@ const recipes = [
   'https://introweb.tech/assets/json/ghostCookies.json',
   'https://introweb.tech/assets/json/birthdayCake.json',
   'https://introweb.tech/assets/json/chocolateChip.json',
-  '/assets/recipes/recipie1.json',
-  '/assets/recipes/recipie2.json',
-  '/assets/recipes/recipie3.json'
+  'assets/recipes/recipie1.json',
+  'assets/recipes/recipie2.json',
+  'assets/recipes/recipie3.json'
 ];
 
 // Once all of the recipes that were specified above have been fetched, their
